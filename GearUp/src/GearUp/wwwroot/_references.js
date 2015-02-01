ec362e16b4b9ab063cd3de7bbf92527825d8d320
Timmy Douglas <timmy@timmyd.info>
2015-02-01 12:44:12 -0500

    delete some stuff

--- GearUp/src/GearUp/wwwroot/_references.js
+++ GearUp/src/GearUp/wwwroot/_references.js
@@ -1,6 +1,2 @@
 ﻿/// <autosync enabled="true" />
 /// <reference path="../gruntfile.js" />
-/// <reference path="../appjs/main.js" />
-/// <reference path="../appjs/home.js" />
-/// <reference path="lib/ember.js" />
-/// <reference path="app/app.js" />
