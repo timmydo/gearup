5e54ee5c069b9a6416299830aef4a4f827b648a2
Timmy Douglas <timmy@timmyd.info>
2015-02-02 22:58:56 -0500

    login code

---
+++ GearUp/src/GearUp/Controllers/LoginController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNet.Mvc;
+using Newtonsoft.Json;
+
+// For more information on enabling Web API for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace GearUp.Controllers.Controllers
+{
+	[Route("api/login")]
+    public class LoginController : Controller
+    {
+
+		class LoginInfo
+		{
+			public string username { get; set; }
+			public string password { get; set; }
+			public string remember { get; set; }
+		}
+
+
+        // GET: api/values
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public JsonResult Post([FromBody]string value)
+        {
+			var li = JsonConvert.DeserializeObject<LoginInfo>(value);
+
+			return Json(new { authResult = "abc123"});
+        }
+
+
+    }
+}
--- GearUp/src/GearUp/app/ts/login.ts
+++ GearUp/src/GearUp/app/ts/login.ts
@@ -1,12 +1,52 @@
 ﻿/// <reference path="app.ts" />
 
+App.LoginCreds = Ember.Object.extend({
+    username: null,
+    password: null,
+    remember: false,
 
-App.LoginRoute = Ember.Route.extend({
-	setupController: function (controller) {
-		// `controller` is the instance of ApplicationController
-		controller.set('title', "Hello world!");
-	}
+    json: function () {
+        return {
+            username: this.get('username'),
+            password: this.get('password'),
+            remember: this.get('remember')
+        }
+    }
 });
 
-App.LoginController = Ember.Controller.extend({
+App.LoginRoute = (<any>Ember.Route).extend(App.Ajax, {
+	model: function () {
+        // let our login template fill in the properties of a creds object
+        return App.LoginCreds.create({});
+    },
+
+    events: {
+        login: function () {
+            var model = this.modelFor('login'); // <App.LoginCreds>
+            var self = this;
+            self.POST('/api/login', model.json()).then(
+                function (json) {
+                    if (json == null) return; // shouldn't happen, but should still NPE protect
+                    if (json.error != null) {
+                        // useful for any ajax call: set the global error alert with our error message
+                        App.set('error', json.error.message);
+                    } else {
+                        // setting this value will reveal our logout button
+                        App.set('authToken', json.authToken);
+                        if (model.get('remember')) {
+                            localStorage['authToken'] = json.authToken;
+                        } else {
+                            // make sure a stale value isn't left behind
+                            delete localStorage['authToken'];
+                        }
+                        // clear out any login error that was left over
+                        App.set('error', null);
+                        self.router.transitionTo('index');
+                    }
+                });
+        }
+    }
+});
+
+App.LoginController = Ember.ObjectController.extend({
 });
