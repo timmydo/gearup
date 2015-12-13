340ad7a28785f4aad628ac1a06f1dab9e944d9d8
Timmy Douglas <timmy@timmyd.info>
2015-12-12 16:19:50 -0800

    add new projects

--- GearUp/Program.cs
+++ GearUp/Program.cs
@@ -1,26 +1,33 @@
 ﻿namespace GearUp
 {
 	using System;
+	using System.Fabric;
 	using Microsoft.AspNet.Hosting;
 	using Microsoft.Extensions.Configuration;
-
+	using System.Diagnostics;
+	using System.Threading;
 	public class Program
 	{
 		public static void Main(string[] args)
 		{
-			//Add command line configuration source to read command line parameters.
-			var builder = new ConfigurationBuilder();
-			builder.AddCommandLine(args);
-			var config = builder.Build();
-
-			using (new WebHostBuilder(config)
-				.UseServerFactory("Microsoft.AspNet.Server.WebListener")
-				.Build()
-				.Start())
+			try
 			{
-				Console.WriteLine("Started the server..");
-				Console.WriteLine("Press any key to stop the server");
-				Console.ReadLine();
+				using (FabricRuntime fabricRuntime = FabricRuntime.Create())
+				{
+					// This is the name of the ServiceType that is registered with FabricRuntime. 
+					// This name must match the name defined in the ServiceManifest. If you change
+					// this name, please change the name of the ServiceType in the ServiceManifest.
+					fabricRuntime.RegisterServiceType("WebType", typeof(Web));
+
+					ServiceEventSource.Current.ServiceTypeRegistered(Process.GetCurrentProcess().Id, typeof(Web).Name);
+
+					Thread.Sleep(Timeout.Infinite);
+				}
+			}
+			catch (Exception e)
+			{
+				ServiceEventSource.Current.ServiceHostInitializationFailed(e);
+				throw;
 			}
 		}
 	}
