8e7559dba343bde9d8c4361096f82c3323844a1e
Timmy Douglas <timmy@timmyd.info>
2015-12-17 00:14:06 -0800

    new interfaces

---
+++ GearUp/Services/ServiceFabricPartitionedKeyValueDictionary.cs
@@ -0,0 +1,65 @@
+﻿
+namespace GearUp.Services
+{
+	using Microsoft.ServiceFabric.Services.Remoting.Client;
+	using Shared.Interfaces;
+	using System;
+	using System.Threading.Tasks;
+
+	public class ServiceFabricPartitionedKeyValueDictionary : IPartitionedKeyValueDictionary
+	{
+		private Uri uri;
+
+
+		private const ulong fnvPrime = unchecked(1099511628211);
+		private const ulong fnvOffsetBasis = unchecked(14695981039346656037);
+
+		static private long getPartitionKey(string key)
+		{
+			ulong h = fnvOffsetBasis;
+			if (string.IsNullOrEmpty(key)) return unchecked((long)h);
+			var arr = key.ToCharArray();
+			for (var i = 0; i < arr.Length; i++)
+			{
+				unchecked
+				{
+					h ^= arr[i];
+					h *= fnvPrime;
+				}
+			}
+
+			return unchecked((long)h);
+		}
+
+
+		public ServiceFabricPartitionedKeyValueDictionary(Uri uri)
+		{
+			this.uri = uri;
+		}
+
+
+		public async Task AddKeyAsync(string key, string value)
+		{
+			var proxy = ServiceProxy.Create<IPartitionedKeyValueDictionary>(getPartitionKey(key), this.uri);
+			await proxy.AddKeyAsync(key, value);
+		}
+
+		public async Task DeleteKeyAsync(string key)
+		{
+			var proxy = ServiceProxy.Create<IPartitionedKeyValueDictionary>(getPartitionKey(key), this.uri);
+			await proxy.DeleteKeyAsync(key);
+		}
+
+		public async Task<string> GetKeyAsync(string key)
+		{
+			var proxy = ServiceProxy.Create<IPartitionedKeyValueDictionary>(getPartitionKey(key), this.uri);
+			return await proxy.GetKeyAsync(key);
+		}
+
+		public async Task UpdateKeyAsync(string key, string value, string updateFunc)
+		{
+			var proxy = ServiceProxy.Create<IPartitionedKeyValueDictionary>(getPartitionKey(key), this.uri);
+			await proxy.UpdateKeyAsync(key, value, updateFunc);
+		}
+	}
+}
---
+++ Shared/Interfaces/IPartitionedKeyValueDictionary.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.ServiceFabric.Services.Remoting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shared.Interfaces
+{
+    public interface IPartitionedKeyValueDictionary : IService
+    {
+		// return string.empty if it doesn't exist
+		Task<string> GetKeyAsync(string key);
+		Task DeleteKeyAsync(string key);
+		Task AddKeyAsync(string key, string value);
+		Task UpdateKeyAsync(string key, string value, string updateMethod);
+    }
+}
